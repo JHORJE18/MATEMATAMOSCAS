05ba595b11948992907055cc120e2d1a39f068f8
Jorge Lopez Gil <wiijlg@hotmail.com>
2017-03-10 19:03:54 +0100

    Clase iniciar ventanas v1.2

--- src/Aplicacion.java
+++ src/Aplicacion.java
@@ -7,30 +7,40 @@ public class Aplicacion {
 
 	public static void main(String[] args){
 		Scanner entrada = new Scanner(System.in);
+		boolean salir= false;
 		
-		System.out.println("Ventana a mostrar");
-		System.out.println("1. Ventana Principal");
-		System.out.println("2. Ventana Ajustes");
-		System.out.println("3. Ventana Puntuaciones");
-		int seleccion = entrada.nextInt();
-		
-		switch (seleccion){
-		case 1:
-			//Ventana Principal
-			Principal ventanaPrincipal = new Principal();
-			ventanaPrincipal.setVisible(true);
-			break;
-		case 2:
-			//Ventana Ajustes
-			Ajustes ventanaAjustes = new Ajustes();
-			ventanaAjustes.setVisible(true);
-			break;
-		case 3:
-			//Ventana Puntuaciones
-			Puntuaciones ventanaPuntuaciones = new Puntuaciones();
-			ventanaPuntuaciones.setVisible(true);
-			break;
+		while (!salir){	
+			System.out.println("Ventana a mostrar");
+			System.out.println("1. Ventana Principal");
+			System.out.println("2. Ventana Ajustes");
+			System.out.println("3. Ventana Puntuaciones");
+			System.out.println("4. Salir");
+			int seleccion = entrada.nextInt();
+			
+			switch (seleccion){
+			case 1:
+				//Ventana Principal
+				Principal ventanaPrincipal = new Principal();
+				ventanaPrincipal.setVisible(true);
+				break;
+			case 2:
+				//Ventana Ajustes
+				Ajustes ventanaAjustes = new Ajustes();
+				ventanaAjustes.setVisible(true);
+				break;
+			case 3:
+				//Ventana Puntuaciones
+				Puntuaciones ventanaPuntuaciones = new Puntuaciones();
+				ventanaPuntuaciones.setVisible(true);
+				break;
+			case 4:
+				//Finalizar
+				salir =true;
+				break;
+			default:
+				System.out.println("Eleccion no valida");
+				break;
+			}
 		}
-		
 	}
 }
