2de4453d41b09475887a31a4efb7950295950574
Jorge Lopez Gil <wiijlg@hotmail.com>
2017-05-05 20:22:36 +0200

    Pantalla Puntuacion

--- src/Ventanas/Puntuaciones.java
+++ src/Ventanas/Puntuaciones.java
@@ -5,6 +5,18 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.BoxLayout;
+import java.awt.Component;
+import java.awt.Color;
+import javax.swing.JList;
+import javax.swing.AbstractListModel;
+import javax.swing.JComboBox;
+import javax.swing.DefaultComboBoxModel;
+import java.awt.FlowLayout;
+import javax.swing.JButton;
+import javax.swing.SwingConstants;
 
 public class Puntuaciones extends JFrame {
 
@@ -30,6 +42,56 @@ public class Puntuaciones extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		
+		JPanel panel = new JPanel();
+		panel.setBackground(Color.LIGHT_GRAY);
+		contentPane.add(panel, BorderLayout.CENTER);
+		panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+		
+		JLabel lblNewLabel_1 = new JLabel("MEJORES DE ESTE NIVEL");
+		lblNewLabel_1.setAlignmentX(Component.CENTER_ALIGNMENT);
+		lblNewLabel_1.setFont(new Font("Krungthep", Font.PLAIN, 18));
+		lblNewLabel_1.setBackground(Color.WHITE);
+		panel.add(lblNewLabel_1);
+		
+		JPanel panel_3 = new JPanel();
+		panel_3.setBackground(Color.WHITE);
+		panel.add(panel_3);
+		panel_3.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+		
+		JList list = new JList();
+		panel_3.add(list);
+		list.setModel(new AbstractListModel() {
+			String[] values = new String[] {"BEST POINTS 1", "BEST POINTS 2", "BEST POINTS 3", "BEST POINTS 4", "BEST POINTS 5", "BEST POINTS 6", "BEST POINTS 7", "BEST POINTS 8"};
+			public int getSize() {
+				return values.length;
+			}
+			public Object getElementAt(int index) {
+				return values[index];
+			}
+		});
+		
+		JPanel panel_2 = new JPanel();
+		panel_2.setBackground(Color.LIGHT_GRAY);
+		panel_2.setAlignmentX(Component.RIGHT_ALIGNMENT);
+		panel.add(panel_2);
+		panel_2.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+		
+		JComboBox comboBox = new JComboBox();
+		panel_2.add(comboBox);
+		comboBox.setModel(new DefaultComboBoxModel(new String[] {"NVL1", "NVL2", "NVL3"}));
+		
+		JButton btnNewButton = new JButton("VOLVER");
+		btnNewButton.setHorizontalAlignment(SwingConstants.RIGHT);
+		panel_2.add(btnNewButton);
+		
+		JPanel panel_1 = new JPanel();
+		panel_1.setBackground(Color.ORANGE);
+		contentPane.add(panel_1, BorderLayout.NORTH);
+		
+		JLabel lblNewLabel = new JLabel("PUNTUACIONES");
+		lblNewLabel.setFont(new Font("Bradley Hand", Font.PLAIN, 31));
+		panel_1.add(lblNewLabel);
 	}
 	//Juanjo mira esto
 }
